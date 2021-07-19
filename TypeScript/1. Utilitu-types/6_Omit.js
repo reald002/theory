3ac9dee21f1d3ce9372b6b1218b80718005db367
Andrew <zebelian.exceedteam@gmail.com>
2021-07-19 17:28:50 +0300

    Add Omit.js

---
+++ TypeScript/1. Utilitu-types/6_Omit.js
@@ -0,0 +1,10 @@
+/** Omit - это новый тип, в котором можно указать свойства, которые будут исключены из исходного типа. */
+var todo = {
+    title: "Clean room",
+    completed: false,
+    createdAt: 1615544252770
+};
+var todoInfo = {
+    title: "Pick up kids",
+    description: "Kindergarten closes at 5pm"
+};
